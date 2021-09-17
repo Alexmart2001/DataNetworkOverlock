1586d146f9a484ca25d68c7e0811fba3ae02c0e9
Alexmart2001 <alexandera2345@gmail.com>
2021-09-17 08:30:33 -0500

    CambiosDao

--- src/main/java/co/edu/usbbog/datanetworkoverlock/controller/dao/NmapDAO.java
+++ src/main/java/co/edu/usbbog/datanetworkoverlock/controller/dao/NmapDAO.java
@@ -1,4 +1,13 @@
 package co.edu.usbbog.datanetworkoverlock.controller.dao;
 
+import co.edu.usbbog.datanetworkoverlock.model.NmapDTO;
+
+import java.util.List;
+
 public interface NmapDAO {
+    public boolean create();
+    public boolean edit();
+    public boolean remove();
+    public NmapDTO find ();
+    public List<NmapDTO> findAll();
 }
--- src/main/java/co/edu/usbbog/datanetworkoverlock/controller/dao/SnortDAO.java
+++ src/main/java/co/edu/usbbog/datanetworkoverlock/controller/dao/SnortDAO.java
@@ -1,4 +1,12 @@
 package co.edu.usbbog.datanetworkoverlock.controller.dao;
+import co.edu.usbbog.datanetworkoverlock.model.SnortDTO;
+
+import java.util.List;
 
 public interface SnortDAO {
+    public boolean create();
+    public boolean edit();
+    public boolean remove();
+    public SnortDTO find ();
+    public List<SnortDTO> findAll();
 }
