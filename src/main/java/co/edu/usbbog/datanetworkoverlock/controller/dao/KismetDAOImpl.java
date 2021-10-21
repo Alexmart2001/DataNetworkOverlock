f332ec6dd42d714d3fdeec19eb98c1a9ed698dde
Alexander Martinez <83377810+Alexmart2001@users.noreply.github.com>
2021-10-21 18:32:01 -0500

    dao

--- src/main/java/co/edu/usbbog/datanetworkoverlock/controller/dao/KismetDAOImpl.java
+++ src/main/java/co/edu/usbbog/datanetworkoverlock/controller/dao/KismetDAOImpl.java
@@ -1,29 +1,147 @@
 package co.edu.usbbog.datanetworkoverlock.controller.dao;
 
+import co.edu.usbbog.datanetworkoverlock.controller.config.Conexion;
+import co.edu.usbbog.datanetworkoverlock.model.IntrusionesDTO;
 import co.edu.usbbog.datanetworkoverlock.model.KismetDTO;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLTimeoutException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class KismetDAOImpl implements KismetDAO{
 
+        private final Conexion conexion;
+
+        public KismetDAOImpl(){
+
+            this.conexion = new Conexion();
+        }
     @Override
     public boolean create(KismetDTO kismet) {
-        return false;
+            boolean seHizo = false;
+            try{
+                String query = "INSERT INTO kismet (id_analisis_kismet, nombre_red_detectada, clientes_detectados, modo_funcionamiento_disp_wifi, "
+                        + "tipo_encriptacion, canal_operacion_red, paquetes_capturados, intrusiones) VALUES ("
+                        + kismet.getIdAnalisisKismet() + ","
+                        + "'"+ kismet.getNombreRedDetectada() + "',"
+                        + "'" + kismet.getClientesDetectados() + "',"
+                        + "'" + kismet.getModoFuncionamientoWifi() + "',"
+                        + "'" + kismet.getTipoEncriptacion() + "',"
+                        + "'" + kismet.getCanalOperacionRed() + "',"
+                        + "'" + kismet.getPaquetesCapturados() + "',"
+                        +       kismet.getIntrusiones().getIdIntrusion()
+                        + ");";
+                System.out.println(query);
+                this.conexion.conectar();
+                Statement stmt = this.conexion.getConnection().createStatement();
+                stmt.executeUpdate(query);
+                stmt.close();
+                this.conexion.desconectar();
+                System.out.println("Se inserto el dato");
+                seHizo = true;
+            } catch (SQLTimeoutException e) {
+                seHizo = false;
+                System.out.println("No se inserto el dato");
+                System.out.println("Causa: " + e.getMessage());
+                System.out.println("Causa: " + e.getSQLState());
+            } catch (SQLException e) {
+                seHizo = false;
+                System.out.println("No se inserto el dato");
+                System.out.println("Causa: " + e.getMessage());
+                System.out.println("Causa: " + e.getSQLState());
+            }
+        return seHizo;
     }
 
     @Override
     public KismetDTO find(Integer id_analisis_kismet) {
-        return null;
+            KismetDTO kismet = null;
+            IntrusionesDTO intrusion;
+            IntrusionesDAO intrusionesDAO = new IntrusionesDAOImpl();
+            try{
+                String query = "SELECT*FROM kismet where id_analisis_kismet = " + id_analisis_kismet + ";";
+                System.out.println(query);
+                this.conexion.conectar();
+                Statement stmt = this.conexion.getConnection().createStatement();
+                ResultSet rs = stmt.executeQuery(query);
+                rs.first();
+                intrusion = intrusionesDAO.find(rs.getInt("instrusiones"));
+                kismet = new KismetDTO(
+                        rs.getInt("id_analisis_kismet"),
+                        rs.getString("nombre_red_detectada"),
+                        rs.getString("clientes_detectados"),
+                        rs.getString("modo_funcionamiento_disp_wifi"),
+                        rs.getString("tipo_encriptacion"),
+                        rs.getString("canal_operacion_red"),
+                        rs.getString("paquetes_capturados"),
+                        intrusion);
+                rs.close();
+                stmt.close();
+                this.conexion.desconectar();
+                System.out.println("Se obtuvo el dato");
+            } catch (SQLTimeoutException e) {
+                kismet = null;
+                System.out.println("No se obtuvo el dato");
+                System.out.println("Causa: " + e.getMessage());
+                System.out.println("Causa: " + e.getSQLState());
+            } catch (SQLException e) {
+                kismet = null;
+                System.out.println("No se obtuvo el dato");
+                System.out.println("Causa: " + e.getMessage());
+                System.out.println("Causa: " + e.getSQLState());
+            }
+        return kismet;
     }
 
     @Override
     public List<KismetDTO> findAll() {
-        return null;
+            List<KismetDTO> kismet = new ArrayList<>();
+            KismetDTO kisme = null;
+            IntrusionesDTO intrusiones;
+            IntrusionesDAO intrusiondao = new IntrusionesDAOImpl();
+            try{
+                String query = "SELECT * FROM kismet;";
+                System.out.println(query);
+                this.conexion.conectar();
+                Statement stmt = this.conexion.getConnection().createStatement();
+                ResultSet rs = stmt.executeQuery(query);
+                while (rs.next()) {
+                    intrusiones = intrusiondao.find(rs.getInt("intrusiones"));
+                    kisme = new KismetDTO(
+                            rs.getInt("id_analisis_kismet"),
+                            rs.getString("nombre_red_detectada"),
+                            rs.getString("clientes_detectados"),
+                            rs.getString("modo_funcionamiento_disp_wifi"),
+                            rs.getString("tipo_encriptacion"),
+                            rs.getString("canal_operacion_red"),
+                            rs.getString("paquetes_capturados"),
+                            intrusiones);
+                    kismet.add(kisme);
+                }
+                rs.close();
+                stmt.close();
+                this.conexion.desconectar();
+                System.out.println("Se obtuvieron los datos");
+            } catch (SQLTimeoutException e) {
+                kismet = new ArrayList<>();
+                System.out.println("No se obtuvieron los datos");
+                System.out.println("Causa: " + e.getMessage());
+                System.out.println("Causa: " + e.getSQLState());
+            } catch (SQLException e) {
+                kismet = new ArrayList<>();
+                System.out.println("No se obtuvieron los datos");
+                System.out.println("Causa: " + e.getMessage());
+                System.out.println("Causa: " + e.getSQLState());
+            }
+        return kismet;
     }
 
     @Override
     public int count() {
-        return 0;
+        return findAll().size();
     }
 
 }
--- src/main/java/co/edu/usbbog/datanetworkoverlock/controller/dao/NmapDAOImpl.java
+++ src/main/java/co/edu/usbbog/datanetworkoverlock/controller/dao/NmapDAOImpl.java
@@ -1,27 +1,160 @@
 package co.edu.usbbog.datanetworkoverlock.controller.dao;
 
+import co.edu.usbbog.datanetworkoverlock.controller.config.Conexion;
+import co.edu.usbbog.datanetworkoverlock.model.BackdoorDTO;
+import co.edu.usbbog.datanetworkoverlock.model.FallosDeRedDTO;
 import co.edu.usbbog.datanetworkoverlock.model.NmapDTO;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLTimeoutException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class NmapDAOImpl implements NmapDAO{
+    private final Conexion conexion;
+
+    public NmapDAOImpl(){
+        this.conexion = new Conexion();
+    }
+
     @Override
     public boolean create(NmapDTO nmap) {
-        return false;
+        boolean seHizo = false;
+        try{
+            String query = "INSERT INTO nmap (id_analisis_nmap, protocolo_puerto, estado, razon, nombre_servicio,"
+                    + "producto, version, puerto_id, informacion_extra, backdoor, fallos_de_red) VALUES ("
+                    + nmap.getIdAnalisisNmap() + ", '"
+                    + nmap.getProtocoloPuerto() + ",'"
+                    + nmap.getEstado() +"','"
+                    + nmap.getRazon() +"','"
+                    + nmap.getNombreServicio() + "','"
+                    + nmap.getProducto() + "','"
+                    + nmap.getVersion() + "',"
+                    + nmap.getPuertoId() + ",'"
+                    + nmap.getInformacionExtra() +"',"
+                    + nmap.getBackdoor().getIdBackdoor() +","
+                    + nmap.getFallosRed().getIdFallo()
+                    + ");";
+            System.out.println(query);
+            this.conexion.conectar();
+            Statement stmt = this.conexion.getConnection().createStatement();
+            stmt.executeUpdate(query);
+            stmt.close();
+            this.conexion.desconectar();
+            System.out.println("Se inserto el dato");
+            seHizo = true;
+        } catch (SQLTimeoutException e) {
+            seHizo = false;
+            System.out.println("No se inserto el dato");
+            System.out.println("Causa: " + e.getMessage());
+            System.out.println("Causa: " + e.getSQLState());
+        } catch (SQLException e) {
+            seHizo = false;
+            System.out.println("No se inserto el dato");
+            System.out.println("Causa: " + e.getMessage());
+            System.out.println("Causa: " + e.getSQLState());
+        }
+        return seHizo;
     }
 
     @Override
     public NmapDTO find(Integer id_analisis_nmap) {
-        return null;
+    NmapDTO nmap = null;
+        BackdoorDTO backdoor;
+        FallosDeRedDTO fallos;
+        BackdoorDAO backdoordao = new BackdoorDAOImpl();
+        FallosDeRedDAO fallosdao = new FallosDeRedDAOImpl();
+        try{
+            String query = "SELECT*FROM nmap WHERE id_analisis_nmap = " + id_analisis_nmap + ";";
+            System.out.println(query);
+            this.conexion.conectar();
+            Statement stmt = this.conexion.getConnection().createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            rs.first();
+            backdoor = backdoordao.find(rs.getInt("backdoor"));
+            fallos = fallosdao.find(rs.getInt("fallos_de_red"));
+            nmap = new NmapDTO(
+                    rs.getInt("id_analisis_nmap"),
+                    rs.getString("protocolo_puerto"),
+                    rs.getString("estado"),
+                    rs.getString("razon"),
+                    rs.getString("nombre_servicio"),
+                    rs.getString("produto"),
+                    rs.getString("version"),
+                    rs.getInt("puerto_id"),
+                    rs.getString("informacion_extra"),
+                    backdoor,
+                    fallos);
+            rs.close();
+            stmt.close();
+            this.conexion.desconectar();
+            System.out.println("Se obtuvo el dato");
+        } catch (SQLTimeoutException e) {
+            nmap = null;
+            System.out.println("No se obtuvo el dato");
+            System.out.println("Causa: " + e.getMessage());
+            System.out.println("Causa: " + e.getSQLState());
+        } catch (SQLException e) {
+            nmap = null;
+            System.out.println("No se obtuvo el dato");
+            System.out.println("Causa: " + e.getMessage());
+            System.out.println("Causa: " + e.getSQLState());
+        }
+        return nmap;
     }
-
     @Override
     public List<NmapDTO> findAll() {
-        return null;
+        List<NmapDTO> nmap = new ArrayList<>();
+        NmapDTO nma =null;
+        BackdoorDTO backdoor;
+        FallosDeRedDTO fallos;
+        BackdoorDAO backdoordao = new BackdoorDAOImpl();
+        FallosDeRedDAO fallosdao = new FallosDeRedDAOImpl();
+        try{
+            String query = "SELECT * FROM nmap;";
+            System.out.println(query);
+            this.conexion.conectar();
+            Statement stmt = this.conexion.getConnection().createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                backdoor = backdoordao.find(rs.getInt("backdoor"));
+                fallos = fallosdao.find(rs.getInt("fallos_de_red"));
+                nma = new NmapDTO(
+                        rs.getInt("id_analisis_nmap"),
+                        rs.getString("protocolo_puerto"),
+                        rs.getString("estado"),
+                        rs.getString("razon"),
+                        rs.getString("nombre_servicio"),
+                        rs.getString("produto"),
+                        rs.getString("version"),
+                        rs.getInt("puerto_id"),
+                        rs.getString("informacion_extra"),
+                        backdoor,
+                        fallos);
+                nmap.add(nma);
+            }
+            rs.close();
+            stmt.close();
+            this.conexion.desconectar();
+            System.out.println("Se obtuvieron los empleados");
+        } catch (SQLTimeoutException e) {
+            nmap = new ArrayList<>();
+            System.out.println("No se obtuvieron los empleados");
+            System.out.println("Causa: " + e.getMessage());
+            System.out.println("Causa: " + e.getSQLState());
+        } catch (SQLException e) {
+            nmap = new ArrayList<>();
+            System.out.println("No se obtuvieron los empleados");
+            System.out.println("Causa: " + e.getMessage());
+            System.out.println("Causa: " + e.getSQLState());
+        }
+        return nmap;
     }
 
     @Override
     public int count() {
-        return 0;
+        return findAll().size();
     }
 }
