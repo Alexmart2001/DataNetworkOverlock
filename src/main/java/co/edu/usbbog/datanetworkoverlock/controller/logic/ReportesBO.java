fd24675a09fc3efd780edcda95d0aa5261477796
David Rojas Jimenez <davidrojasj@gmail.com>
2021-10-21 20:23:19 -0500

    BO BO Nmap BO Nessus BO Snort BO RegistroCambios BO Reportes DTO Nmap (especificar coorrectamente el ultimo constructor, ya que las variables que representan a las llaves foraneas dde esta tabla estaban mas nombradas)

--- src/main/java/co/edu/usbbog/datanetworkoverlock/controller/logic/ReportesBO.java
+++ src/main/java/co/edu/usbbog/datanetworkoverlock/controller/logic/ReportesBO.java
@@ -1,4 +1,28 @@
 package co.edu.usbbog.datanetworkoverlock.controller.logic;
 
+import co.edu.usbbog.datanetworkoverlock.controller.dao.ReportesDAO;
+import co.edu.usbbog.datanetworkoverlock.controller.dao.ReportesDAOImpl;
+import co.edu.usbbog.datanetworkoverlock.model.IntrusionesDTO;
+import co.edu.usbbog.datanetworkoverlock.model.ReportesDTO;
+import co.edu.usbbog.datanetworkoverlock.model.SnortDTO;
+
+import java.util.Calendar;
+import java.util.List;
+
 public class ReportesBO {
+    private final ReportesDAO reportesDAO;
+
+    public ReportesBO() {this.reportesDAO = new ReportesDAOImpl();}
+
+    public String crearReportes(Integer idReporte, String nombreReporte, String tipoReporte, String descripcion, Calendar fecha){
+        return reportesDAO.create(new ReportesDTO(idReporte, nombreReporte, tipoReporte, descripcion, fecha)) ? "Creado" : "No se creó";
+    }
+
+    public ReportesDTO buscarReportes (Integer idReporte){
+        return reportesDAO.find(idReporte);
+    }
+
+    public List<ReportesDTO> mostrarReportes() {
+        return reportesDAO.findAll();
+    }
 }
--- src/main/java/co/edu/usbbog/datanetworkoverlock/controller/logic/SnortBO.java
+++ src/main/java/co/edu/usbbog/datanetworkoverlock/controller/logic/SnortBO.java
@@ -1,4 +1,34 @@
 package co.edu.usbbog.datanetworkoverlock.controller.logic;
 
+import co.edu.usbbog.datanetworkoverlock.controller.dao.PersonaDAOImpl;
+import co.edu.usbbog.datanetworkoverlock.controller.dao.SnortDAO;
+import co.edu.usbbog.datanetworkoverlock.controller.dao.SnortDAOImpl;
+import co.edu.usbbog.datanetworkoverlock.model.IntrusionesDTO;
+import co.edu.usbbog.datanetworkoverlock.model.PersonaDTO;
+import co.edu.usbbog.datanetworkoverlock.model.SnortDTO;
+
+import java.util.Calendar;
+import java.util.List;
+
+;
+
 public class SnortBO {
+
+    private final SnortDAO snortDAO;
+
+    public SnortBO(){
+        this.snortDAO = new SnortDAOImpl();
+    }
+
+    public String crearSnort(Integer id_analisis_snort, Calendar fecha, Calendar hora, String prioridad, String nombre_host, String mensaje, IntrusionesDTO intrusionesDTO){
+        return snortDAO.create(new SnortDTO(id_analisis_snort, fecha, hora, prioridad, nombre_host, mensaje, intrusionesDTO)) ? "Creado" : "No se creó";
+    }
+
+    public SnortDTO buscarSnortDTO (Integer id_analisis_snort){
+        return snortDAO.find(id_analisis_snort);
+    }
+
+    public List<SnortDTO> mostrarSnort() {
+        return snortDAO.findAll();
+    }
 }
