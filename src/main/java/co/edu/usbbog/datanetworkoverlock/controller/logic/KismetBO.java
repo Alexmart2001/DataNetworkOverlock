7960d329b2180326a14c943c42464d571bca20da
Alexander Martinez <83377810+Alexmart2001@users.noreply.github.com>
2021-10-22 07:32:16 -0500

    bobada

--- src/main/java/co/edu/usbbog/datanetworkoverlock/controller/logic/KismetBO.java
+++ src/main/java/co/edu/usbbog/datanetworkoverlock/controller/logic/KismetBO.java
@@ -14,14 +14,13 @@ public class KismetBO {
         this.kismetd = new KismetDAOImpl();
     }
 
-    public String crearKismet(Integer id_analisis_kismet, String nombre_red_detectada, String clientes_detectados, String modo_funcionamiento_disp_wifi, String tipo_encriptacion, String canal_operacion_red, String paquetes_capturados, IntrusionesDTO intrusiones){
-        return kismetd.create(new KismetDTO(id_analisis_kismet,nombre_red_detectada,clientes_detectados,modo_funcionamiento_disp_wifi,tipo_encriptacion,canal_operacion_red,paquetes_capturados,intrusiones)) ? "creado" : "no se creo";
-
+    public String crearKismet(Integer id_analisis_kismet, String nombre_red_detectada, String clientes_detectados, String modo_funcionamiento_disp_wifi, String tipo_encriptacion, String canal_operacion_red, String paquetes_capturados, IntrusionesDTO intrusiones) {
+        return kismetd.create(new KismetDTO(id_analisis_kismet, nombre_red_detectada, clientes_detectados, modo_funcionamiento_disp_wifi, tipo_encriptacion, canal_operacion_red, paquetes_capturados, intrusiones)) ? "creado" : "no se creo";
+    }
         public KismetDTO buscarKismet(Integer id_analisis_kismet){
             return kismetd.find(id_analisis_kismet);
         }
         public List<KismetDTO> mostrarKismets(){
             return kismetd.findAll();
         }
-    }
 }
