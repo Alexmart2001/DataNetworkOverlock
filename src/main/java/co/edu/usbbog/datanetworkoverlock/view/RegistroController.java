c9a41fc47d26a76f7a72520b60d4381c9391bcc1
Alexander Martinez <83377810+Alexmart2001@users.noreply.github.com>
2021-10-29 01:04:53 -0500

    registro

--- src/main/java/co/edu/usbbog/datanetworkoverlock/view/RegistroController.java
+++ src/main/java/co/edu/usbbog/datanetworkoverlock/view/RegistroController.java
@@ -1,11 +1,13 @@
 package co.edu.usbbog.datanetworkoverlock.view;
 
+import co.edu.usbbog.datanetworkoverlock.controller.logic.PersonaBO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
@@ -28,7 +30,18 @@ public class RegistroController {
     private Scene scene;
     @FXML
     private TextField Tf_Nombre;
+    @FXML
+    private TextField Tf_Apellido;
+    @FXML
+    private TextField Tf_correo;
+    @FXML
+    private TextField Tf_usuario;
+    @FXML
+    private PasswordField PF_Password;
+    @FXML
+    private PasswordField PR_Password_2;
 
+        PersonaBO registro;
 
     @FXML
     public void cerrar() {
@@ -46,18 +59,33 @@ public class RegistroController {
 
     @FXML
     public void Switch1(ActionEvent event) throws IOException {
-        
+
+        String name = Tf_Nombre.getText();
+        String ape = Tf_Apellido.getText();
+        String cor = Tf_correo.getText();
+        String usu = Tf_usuario.getText();
+        String pas = PF_Password.getText();
+        String contra = PR_Password_2.getText();
+
+        if (pas.equals(contra)){
+            System.out.println(registrar(name, ape, cor, usu, pas));
+
+        }else {
+            System.out.println("Las contraseñas no coinciden");
+        }
 
         root = FXMLLoader.load(getClass().getResource("inicioSesion.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene((Parent) root);
         stage.setScene(scene);
         stage.show();
+
+
     }
 
     @FXML
-    public String registrar (){
-
+    public String registrar (String Tf_Nombre, String Tf_Apellido, String Tf_correo, String Tf_usuario, String PF_Password  ){
+        return registro.crearPersona(Tf_usuario, Tf_Nombre, Tf_Apellido, PF_Password,Tf_correo);
     }
 
 }
